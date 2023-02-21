9d746d3c4fe5e29c9602e341f763d683c7655bf0
Gunjit-V <gunjitvalechha25@live.com>
2023-02-22 03:42:47 +0530

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ import cors from 'cors'
 import * as dotenv from 'dotenv'
 import { Router } from "express";
 import adminRouter from "./routes/AdminRoute.js";
+import mongoose from "mongoose";
 dotenv.config()
 
 const app = express()
+mongoose.connect("mongodb://localhost:27017/Study_Center", { useNewUrlParser: true })
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -16,10 +20,12 @@ app.use(morgan("dev"))
 app.use('/', adminRouter)
 
 
+
 app.use((err, req, res, next) => {
     console.error(err)
     res.status(401).json({ message: "Error occured" })
 })
 app.listen(process.env.PORT, () => {
+
     console.log(`Server running on ${process.env.PORT}`)
 })
