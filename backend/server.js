0487a5e24a5e1ae2c570e9198f21dfc7615585aa
Ayush Kuchya <kuchyaayush@gmail.com>
2023-02-22 03:03:01 +0530

    merge conflict aaega

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan("dev"))
 
-app.use('/admin', adminRouter)
+app.use('/admin_', adminRouter)
 
 
 app.use((err, req, res, next) => {
