9f3d52aab22fd53c08aa48335df43fb25114091e
Gunjit-V <gunjitvalechha25@live.com>
2023-02-22 03:10:14 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan("dev"))
 
-app.use('/admin_', adminRouter)
+app.use('/', adminRouter)
 
 
 app.use((err, req, res, next) => {
